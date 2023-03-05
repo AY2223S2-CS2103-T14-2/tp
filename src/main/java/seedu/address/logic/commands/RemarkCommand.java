609fcf899e3fc8e0999014602443d00599ff3638
Daniel-Kur <daniel.classroom2003@gmail.com>
2023-03-05 13:54:33 +0800

    Fix tutorial

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,19 +1,32 @@
 package seedu.address.logic.commands;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
 
 /**
  * Changes the remark of an existing person in the address book.
  */
 public class RemarkCommand extends Command {
+    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
+    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
 
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
-
+    public static final String MESSAGE_USAGE = "Remark"
+            + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + "remark" + " 1 "
+            + "r/ Likes to swim.";
     private final Index index;
     private final Remark remark;
 
--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,97 +1,57 @@
-package seedu.address.logic.parser;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
-import static seedu.address.testutil.Assert.assertThrows;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.RemarkCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Person;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Remark;
-import seedu.address.testutil.EditPersonDescriptorBuilder;
-import seedu.address.testutil.PersonBuilder;
-import seedu.address.testutil.PersonUtil;
-public class AddressBookParserTest {
- private final AddressBookParser parser = new AddressBookParser();
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ */
+public class RemarkCommandTest {
+ private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
  @Test
- public void parseCommand_add() throws Exception {
-  Person person = new PersonBuilder().build();
-  AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
-  assertEquals(new AddCommand(person), command);
- }
- @Test
- public void parseCommand_clear() throws Exception {
-  assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
-  assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
- }
- @Test
- public void parseCommand_delete() throws Exception {
-  DeleteCommand command = (DeleteCommand) parser.parseCommand(
-          DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
-  assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
- }
- @Test
- public void parseCommand_edit() throws Exception {
-  Person person = new PersonBuilder().build();
-  EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
-  EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
-          + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-  assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
- }
- @Test
- public void parseCommand_exit() throws Exception {
-  assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
-  assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
- }
- @Test
- public void parseCommand_find() throws Exception {
-  List<String> keywords = Arrays.asList("foo", "bar", "baz");
-  FindCommand command = (FindCommand) parser.parseCommand(
-          FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
-  assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
- }
- @Test
- public void parseCommand_help() throws Exception {
-  assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD) instanceof HelpCommand);
-  assertTrue(parser.parseCommand(HelpCommand.COMMAND_WORD + " 3") instanceof HelpCommand);
- }
- @Test
- public void parseCommand_list() throws Exception {
-  assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
-  assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+ public void execute() {
+  final String remark = "Some remark";
+
+  assertCommandFailure(new RemarkCommand(INDEX_FIRST_PERSON, new Remark(remark)), model,
+          String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), remark));
  }
 
  @Test
- public void parseCommand_remark() throws Exception {
-  final Remark remark = new Remark("Some remark.");
-  RemarkCommand command = (RemarkCommand) parser.parseCommand(RemarkCommand.COMMAND_WORD + " "
-          + INDEX_FIRST_PERSON.getOneBased() + " " + PREFIX_REMARK + remark.value);
-  assertEquals(new RemarkCommand(INDEX_FIRST_PERSON, remark), command);
- }
+ public void equals() {
+  final RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_AMY));
 
- @Test
- public void parseCommand_unrecognisedInput_throwsParseException() {
-  assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), ()
-          -> parser.parseCommand(""));
- }
- @Test
- public void parseCommand_unknownCommand_throwsParseException() {
-  assertThrows(ParseException.class, MESSAGE_UNKNOWN_COMMAND, () -> parser.parseCommand("unknownCommand"));
+  // same values -> returns true
+  RemarkCommand commandWithSameValues = new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_AMY));
+  assertTrue(standardCommand.equals(commandWithSameValues));
+
+  // same object -> returns true
+  assertTrue(standardCommand.equals(standardCommand));
+
+  // null -> returns false
+  assertFalse(standardCommand.equals(null));
+
+  // different types -> returns false
+  assertFalse(standardCommand.equals(new ClearCommand()));
+
+  // different index -> returns false
+  assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON, new Remark(VALID_REMARK_AMY))));
+
+  // different remark -> returns false
+  assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, new Remark(VALID_REMARK_BOB))));
  }
 }
